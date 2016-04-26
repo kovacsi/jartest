f73b2d8f873e9c7a80ac5e60b618a830a6f0508c
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-04-26 15:18:53 +0200

    no message

---
+++ JSONValue.m
@@ -0,0 +1,13 @@
+//
+//  JSONValue.m
+//  JarBinder
+//
+//  Created by kovacsi on 26/04/16.
+//  Copyright © 2016 Migeran Ltd. All rights reserved.
+//
+
+#import "JSONValue.h"
+
+@implementation JSONValue
+
+@end
