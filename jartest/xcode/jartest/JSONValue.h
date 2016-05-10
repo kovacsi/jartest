4339ac8641f5833722e8d12781be061fe5503f25
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-05-10 12:55:00 +0200

    no message

---
+++ jartest/xcode/jartest/JSONValue.h
@@ -0,0 +1,15 @@
+//
+//  JSONValue.h
+//  jartest
+//
+//  Created by kovacsi on 26/04/16.
+//  Copyright © 2016 Migeran Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONValue : NSObject
+
++ (NSString*) escape:(NSString*)s;
+
+@end
---
+++ jartest/xcode/jartest/JSONValue.m
@@ -0,0 +1,15 @@
+//
+//  JSONValue.m
+//  jartest
+//
+//  Created by kovacsi on 26/04/16.
+//  Copyright © 2016 Migeran Ltd. All rights reserved.
+//
+
+#import "JSONValue.h"
+
+@implementation JSONValue
++ (NSString*) escape:(NSString*)s {
+    return nil;
+}
+@end
