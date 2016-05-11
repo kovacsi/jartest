daa62bfc03a465aa40b0399e3807a5e846213b95
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-05-11 21:46:25 +0200

    no message

---
+++ jartest/xcode/jartest/AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.h
+//  JarBinder
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jartest/xcode/jartest/AppDelegate.m
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.m
+//  JarBinder
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
