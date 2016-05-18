81093da61d43308283cb094472ab38c2191510a0
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-05-18 14:15:36 +0200

    no message

--- jartest/src/main/java/com/mattakiskft/jartest/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mattakiskft.jartest;
-
-import ios.uikit.c.UIKit;
-
-public class Main {
-    public static void main(String[] args) {
-        UIKit.UIApplicationMain(0, null, null, "AppDelegate");
-    }
-}
